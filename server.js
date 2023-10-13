e639e63b78e50a630f2a06610503a9e5ca518790
TannerJ0nes <127117143+TannerJ0nes@users.noreply.github.com>
2023-10-13 17:37:53 -0230

    Your commit message here

--- server.js
+++ server.js
@@ -1,42 +1,29 @@
-// Import required modules
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const url = require('url');
 
-// Create an HTTP server
 const server = http.createServer((request, response) => {
-  // Parse the requested URL
-  const parsedUrl = url.parse(request.url, true);
-  
-  // Implement routing logic
-  switch (parsedUrl.pathname) {
-    case '/about':
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('About Page');
-      console.log('About page accessed');
-      break;
-    case '/contact':
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('Contact Page');
-      console.log('Contact page accessed');
-      break;
-    case '/products':
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('Products Page');
-      console.log('Products page accessed');
-      break;
-    case '/subscribe':
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('Subscribe Page');
-      console.log('Subscribe page accessed');
-      break;
-    default:
-      response.writeHead(404, { 'Content-Type': 'text/plain' });
-      response.end('Page Not Found');
-      console.log('Page Not Found');
-  }
+    const parsedUrl = url.parse(request.url, true);
+    const pathname = parsedUrl.pathname;
+    let filePath = path.join(__dirname, 'views', (pathname === '/' ? 'index.html' : pathname.slice(1) + '.html'));
+
+
+    console.log('Request URL:', request.url);
+    console.log('File Path:', filePath);
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            response.writeHead(404, { 'Content-Type': 'text/plain' });
+            response.end('Page Not Found');
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(data);
+        }
+    });
 });
 
-// Set the server to listen on port 3000
 server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 3000');
 });
