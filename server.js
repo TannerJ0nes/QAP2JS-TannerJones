807278a97ddf1b13959bb933c27da65798f5129b
TannerJ0nes <127117143+TannerJ0nes@users.noreply.github.com>
2023-10-15 17:11:29 -0230

    Final Functional Update before css

--- server.js
+++ server.js
@@ -2,28 +2,83 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+function createLogFiles() {
+    const currentDate = new Date().toISOString().slice(0, 10);
+    fs.writeFileSync(`specific_route_logs_${currentDate}.txt`, '');
+    // Create other log files if needed
+}
+
+function initializeLogging() {
+    const currentDate = new Date().toISOString().slice(0, 10);
+    if (!fs.existsSync(`specific_route_logs_${currentDate}.txt`)) {
+        createLogFiles();
+    }
+    // Initialize other log files similarly if needed
+}
+
+initializeLogging();
 
 const server = http.createServer((request, response) => {
     const parsedUrl = url.parse(request.url, true);
     const pathname = parsedUrl.pathname;
-    let filePath = path.join(__dirname, 'views', (pathname === '/' ? 'index.html' : pathname.slice(1) + '.html'));
+    let filePath = './views' + (pathname === '/' ? '/index.html' : pathname + '.html');
 
-
-    console.log('Request URL:', request.url);
-    console.log('File Path:', filePath);
+    if (pathname === '/about') {
+        myEmitter.emit('specific_route_accessed', pathname);
+    } else if (pathname !== '/') {
+        myEmitter.emit('non_home_route_accessed', pathname);
+    }
 
     fs.readFile(filePath, 'utf-8', (err, data) => {
         if (err) {
-            console.error('Error reading file:', err);
+            console.error(err);
             response.writeHead(404, { 'Content-Type': 'text/plain' });
             response.end('Page Not Found');
+            myEmitter.emit('http_status', 404);
+            myEmitter.emit('error_warning', 'File not found: ' + filePath);
         } else {
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.end(data);
+            myEmitter.emit('http_status', 200);
+            myEmitter.emit('file_read_success', filePath);
         }
     });
 });
 
 server.listen(3000, () => {
     console.log('Server is listening on port 3000');
+});
+
+myEmitter.on('specific_route_accessed', (route) => {
+    const logMessage = `Specific route accessed: ${route}\n`;
+    console.log(logMessage);
+    fs.appendFileSync(`specific_route_logs_${new Date().toISOString().slice(0, 10)}.txt`, logMessage);
+});
+
+myEmitter.on('non_home_route_accessed', (route) => {
+    const logMessage = `Non-home route accessed: ${route}\n`;
+    console.log(logMessage);
+    fs.appendFileSync(`non_home_route_logs_${new Date().toISOString().slice(0, 10)}.txt`, logMessage);
+});
+
+myEmitter.on('http_status', (statusCode) => {
+    const logMessage = `HTTP Status Code: ${statusCode}\n`;
+    console.log(logMessage);
+    fs.appendFileSync(`http_status_logs_${new Date().toISOString().slice(0, 10)}.txt`, logMessage);
+});
+
+myEmitter.on('error_warning', (message) => {
+    const logMessage = `Error/Warning: ${message}\n`;
+    console.log(logMessage);
+    fs.appendFileSync(`error_warning_logs_${new Date().toISOString().slice(0, 10)}.txt`, logMessage);
+});
+
+myEmitter.on('file_read_success', (filePath) => {
+    const logMessage = `File read successfully: ${filePath}\n`;
+    console.log(logMessage);
+    fs.appendFileSync(`file_read_logs_${new Date().toISOString().slice(0, 10)}.txt`, logMessage);
 });
