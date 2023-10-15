59702382129431f6c2e6d16b82a86e35b90eadca
TannerJ0nes <127117143+TannerJ0nes@users.noreply.github.com>
2023-10-15 17:43:52 -0230

    Css Working

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const myEmitter = new EventEmitter();
 function createLogFiles() {
     const currentDate = new Date().toISOString().slice(0, 10);
     fs.writeFileSync(`specific_route_logs_${currentDate}.txt`, '');
-    // Create other log files if needed
 }
 
 function initializeLogging() {
@@ -17,7 +16,6 @@ function initializeLogging() {
     if (!fs.existsSync(`specific_route_logs_${currentDate}.txt`)) {
         createLogFiles();
     }
-    // Initialize other log files similarly if needed
 }
 
 initializeLogging();
@@ -25,28 +23,41 @@ initializeLogging();
 const server = http.createServer((request, response) => {
     const parsedUrl = url.parse(request.url, true);
     const pathname = parsedUrl.pathname;
-    let filePath = './views' + (pathname === '/' ? '/index.html' : pathname + '.html');
 
-    if (pathname === '/about') {
-        myEmitter.emit('specific_route_accessed', pathname);
-    } else if (pathname !== '/') {
-        myEmitter.emit('non_home_route_accessed', pathname);
-    }
-
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('Page Not Found');
-            myEmitter.emit('http_status', 404);
-            myEmitter.emit('error_warning', 'File not found: ' + filePath);
-        } else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(data);
-            myEmitter.emit('http_status', 200);
-            myEmitter.emit('file_read_success', filePath);
+    if (pathname === '/styles.css') {
+        const cssPath = path.join(__dirname, 'views', 'styles.css');
+        fs.readFile(cssPath, 'utf-8', (err, data) => {
+            if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('File Not Found');
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/css' });
+                response.end(data);
+            }
+        });
+    } else {
+        let filePath = './views' + (pathname === '/' ? '/index.html' : pathname + '.html');
+        if (pathname === '/about') {
+            myEmitter.emit('specific_route_accessed', pathname);
+        } else if (pathname !== '/') {
+            myEmitter.emit('non_home_route_accessed', pathname);
         }
-    });
+
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('Page Not Found');
+                myEmitter.emit('http_status', 404);
+                myEmitter.emit('error_warning', 'File not found: ' + filePath);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(data);
+                myEmitter.emit('http_status', 200);
+                myEmitter.emit('file_read_success', filePath);
+            }
+        });
+    }
 });
 
 server.listen(3000, () => {
